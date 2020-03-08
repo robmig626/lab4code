db0bd4eb7e138a6963b25caaaf38cdaab91901c5
JohnForssander <57511024+JohnForssander@users.noreply.github.com>
2020-03-08 13:57:25 -0700

    added state machine control file

---
+++ state_machine_control.sv
@@ -0,0 +1,14 @@
+module state_machine_control
+(
+    clk,
+    rst,
+    start_init,
+    finish_init,
+    start_shuffle,
+    finish_shuffle,
+    write_enable_init,
+    write_enable_shuffle,
+    address_init,
+    address_shuffle,
+    
+);
