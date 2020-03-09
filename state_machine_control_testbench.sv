a2b9b6572904083be29770d561fb85c328601682
JohnForssander <57511024+JohnForssander@users.noreply.github.com>
2020-03-09 14:24:22 -0700

    updated testbench

--- state_machine_control_testbench.sv
+++ state_machine_control_testbench.sv
@@ -11,3 +11,42 @@ wire write_enable_out;
 wire [7:0] address_out;
 wire [7:0] write_data_out;
 
+state_machine_control dut
+(
+    .clk(clk),
+    .reset(reset),
+    .start(start),
+    .finish(finish),
+    .start_init(start_init),
+    .finish_init(finish_init),
+    .start_shuffle(start_shuffle),
+    .finish_shuffle(finish_shuffle),
+    .write_enable_init(write_enable_init),
+    .write_enable_shuffle(write_enable_shuffle),
+    .write_enable_out(write_enable_out),
+    .address_init(address_init),
+    .address_shuffle(address_shuffle),
+    .address_out(address_out),
+    .write_data_init(write_data_init),
+    .write_data_shuffle(write_data_shuffle),
+    .write_data_out(write_data_out)
+);
+
+initial 
+ begin
+	forever 
+     begin
+	     clk = 1'b0;
+		 #5;
+	     clk = 1'b1;
+		 #5;
+	 end
+ end
+
+ initial 
+  begin
+     reset = 1'b1;
+     start = 1'b1;
+
+  end
+
