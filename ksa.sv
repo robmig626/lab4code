94747a57db9260f832bd4efb49de77f6d4e68016
JohnForssander <57511024+JohnForssander@users.noreply.github.com>
2020-03-09 13:22:06 -0700

    added modules to ksa

--- ksa.sv
+++ ksa.sv
@@ -9,23 +9,51 @@ module ksa (input CLOCK_50,
     output HEX4[6:0],
     output HEX5[6:0]);
 
-    wire write_enable;
-    wire q;
+    wire clk;
+    wire q, write_enable;
     wire [7:0] address, write_data;
+
+    wire start_init, finish_init, write_enable_init;
+    wire [7:0] address_init, write_data_init;
+
+    wire start_shuffle, finish_shuffle;
+    wire read_s, read_key, sum_en, wr_en_si, addr_to_sj, wr_en_sj, swap_en, inc_en;
+
+    assign clk = CLOCK_50;
 
     FSM_mem_w_init FSM1
     (
-        .clk(CLOCK_50),
-        .start(1'b1),
-        .wr_en(write_enable),
-        .mem_addr(address),
-        .wr_data(write_data)
+        .clk(clk),
+        .rst(1'b0),
+        .start(start_init),
+        .finish(finish_init),
+        .wr_en(write_enable_init),
+        .mem_addr(address_init),
+        .wr_data(write_data_init)
     );
+
+    FSM_shuffle_mem FSM2
+    (
+        .start(start_shuffle),
+        .fin(finish_shuffle),
+        .clk(clk),
+        .reset(1'b0),
+        .read_s(read_s),
+        .read_key(read_key),
+        .sum_en(sum_en),
+        .wr_en_si(wr_en_si),
+        .addr_to_sj(addr_to_sj),
+        .wr_en_sj(wr_en_sj),
+        .swap_en(swap_en),
+        .inc_en(inc_en)
+    );
+
+
 
     s_memory mem
     (
         .address(address),
-        .clock(CLOCK_50),
+        .clock(clk),
         .data(write_data),
         .wren(write_enable),
         .q(q)
