75e8654559ad5c9964d2ef715bd480a16f1c5d58
JohnForssander <57511024+JohnForssander@users.noreply.github.com>
2020-03-08 19:43:53 -0700

    a bit more work done on control state machine

--- state_machine_control.sv
+++ state_machine_control.sv
@@ -10,5 +10,13 @@ module state_machine_control
     write_enable_shuffle,
     address_init,
     address_shuffle,
-    
+    address_out,
+    write_data_init,
+    write_data_shuffle,
+    write_enable_out
 );
+
+input clk, rst;
+input finish_init, finish_shuffle;
+input write_data_init, write_enable_shuffle;
+input [7:0] 
